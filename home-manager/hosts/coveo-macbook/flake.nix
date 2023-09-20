8786de7974f1adb71170ed6c60f899df5e325f28
Boris Bera <bbera@coveo.com>
2023-09-20 07:32:47 -0400

    Add macbook config

---
+++ home-manager/hosts/coveo-macbook/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Home Manager configuration of bbera";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    cdo.url = "github:dotboris/cdo/main";
+  };
+
+  outputs = {
+    nixpkgs,
+    home-manager,
+    cdo,
+    ...
+  }: let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    formatter.${system} = pkgs.alejandra;
+
+    homeConfigurations."bbera" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [
+        ./home.nix
+        ./git.nix
+      ];
+
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+      extraSpecialArgs = {
+        cdo = cdo.packages.${system}.default;
+      };
+    };
+  };
+}
---
+++ home-manager/hosts/coveo-macbook/git.nix
@@ -0,0 +1,54 @@
+{pkgs, ...}: {
+  programs.git = {
+    userName = "Boris Bera";
+    userEmail = "bbera@coveo.com";
+
+    extraConfig = {
+      credential."https://github.com".helper = "${pkgs.gh}/bin/gh auth git-credential";
+    };
+
+    ignores = [
+      "__scratch/"
+      ".alt.toml"
+
+      # mac stuff
+      ".DS_Store"
+      ".AppleDouble"
+      ".LSOverride"
+      "Icon"
+      "._*"
+      ".DocumentRevisions-V100"
+      ".fseventsd"
+      ".Spotlight-V100"
+      ".TemporaryItems"
+      ".Trashes"
+      ".VolumeIcon.icns"
+      ".com.apple.timemachine.donotpresent"
+      ".AppleDB"
+      ".AppleDesktop"
+      "Network Trash Folder"
+      "Temporary Items"
+      ".apdisk"
+
+      # Vim stuff
+      "[._]*.s[a-v][a-z]"
+      "[._]*.sw[a-p]"
+      "[._]s[a-rt-v][a-z]"
+      "[._]ss[a-gi-z]"
+      "[._]sw[a-p]"
+      "Session.vim"
+      "Sessionx.vim"
+      ".netrwhist"
+      "*~"
+      "tags"
+      "[._]*.un~"
+
+      # vscode
+      ".vscode"
+      "*.code-workspace"
+      ".history/"
+      ".history"
+      ".ionide"
+    ];
+  };
+}
