c308742e332b43829f608f52a44bace02572ba2e
Boris Bera <beraboris@gmail.com>
2025-01-24 15:12:00 -0500

    feat: add btop

--- home-manager/modules/base.nix
+++ home-manager/modules/base.nix
@@ -2,4 +2,13 @@
 {...}: {
   programs.man.enable = true;
   news.display = "silent";
+  
+  programs.btop = {
+    enable = true;
+    settings = {
+      vim_keys = true;
+      proc_tree = true;
+      proc_gradient = false;
+    };
+  };
 }
