87aa260db08219bec44dedce379ecda568bbb479
Boris Bera <beraboris@gmail.com>
2025-11-30 13:57:52 -0500

    feat(llm): add openwebui

--- modules/llms.nix
+++ modules/llms.nix
@@ -1,6 +1,7 @@
 {...}: {
   flake.nixosModules.llms = {pkgs, ...}: {
     services.ollama.enable = true;
+    services.open-webui.enable = true;
 
     environment.systemPackages = [
       pkgs.opencode
