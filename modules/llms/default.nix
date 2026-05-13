9c31bc06eefec67f3f8c56952fda92792c866f80
Boris Bera <beraboris@gmail.com>
2026-05-12 21:37:50 -0400

    chore(llms): simplify module

--- modules/llms/default.nix
+++ modules/llms/default.nix
@@ -1,32 +1,26 @@
-{
-  inputs,
-  moduleWithSystem,
-  ...
-}: {
-  flake.nixosModules.llms = moduleWithSystem (
-    {system, ...}: {
-      lib,
-      config,
-      pkgs,
-      ...
-    }: let
-      cfg = config.local.llms;
-    in {
-      options.local.llms = {
-        enable = lib.mkEnableOption "LLM tooling";
-        enableOllama = lib.mkEnableOption "Local LLMs through Ollama";
-      };
+{...}: {
+  flake.nixosModules.llms = {
+    lib,
+    config,
+    pkgs,
+    ...
+  }: let
+    cfg = config.local.llms;
+  in {
+    options.local.llms = {
+      enable = lib.mkEnableOption "LLM tooling";
+      enableOllama = lib.mkEnableOption "Local LLMs through Ollama";
+    };
 
-      config = lib.mkIf cfg.enable {
-        nixpkgs.allowUnfreePackages = ["open-webui"];
+    config = lib.mkIf cfg.enable {
+      nixpkgs.allowUnfreePackages = ["open-webui"];
 
-        services.ollama.enable = cfg.enableOllama;
-        services.open-webui.enable = true;
+      services.ollama.enable = cfg.enableOllama;
+      services.open-webui.enable = true;
 
-        environment.systemPackages = [
-          pkgs.opencode
-        ];
-      };
-    }
-  );
+      environment.systemPackages = [
+        pkgs.opencode
+      ];
+    };
+  };
 }
