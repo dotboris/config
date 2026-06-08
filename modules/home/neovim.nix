a46e717c146d125c961509212aef25514f833ea6
Boris Bera <beraboris@gmail.com>
2026-06-08 19:00:38 -0400

    feat(nvim) configure neovim with sane defaults

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -5,9 +5,37 @@
       defaultEditor = true;
       vimAlias = true;
       vimdiffAlias = true;
+      initLua = ''
+        -- show line numbers
+        vim.wo.number = true
+        vim.wo.relativenumber = true
 
-      extraConfig = ''
-        set number
+        -- indent with 2 spaces
+        vim.bo.expandtab = true
+        vim.bo.tabstop = 2
+        vim.bo.shiftwidth = 2
+        vim.bo.autoindent = true
+        vim.bo.smartindent = true
+
+        -- search
+        vim.o.ignorecase = true
+        vim.o.smartcase = true
+        vim.o.hlsearch = true
+        vim.o.incsearch = true
+
+        -- display
+        vim.o.list = true -- show whitespace and the like
+        vim.opt.listchars = {
+          tab = "» ",
+          trail = "·",
+          space = "·",
+          nbsp = "␣",
+        }
+        vim.wo.cursorline = true -- highlight line at cursor
+
+        -- interaction
+        vim.opt.clipboard = "unnamedplus"
+        vim.o.mouse = "a"
       '';
     };
   };
