89f9157abffae6e2742cb096cb56fe1570ef7586
Boris Bera <beraboris@gmail.com>
2026-08-05 13:38:18 -0400

    chore: update

--- modules/llms/packages/mcp-searxng.nix
+++ modules/llms/packages/mcp-searxng.nix
@@ -2,14 +2,14 @@
   perSystem = {pkgs, ...}: {
     packages.mcp-searxng = pkgs.buildNpmPackage (finalAttrs: {
       pname = "mcp-searxng";
-      version = "1.11.1";
+      version = "1.14.0";
       src = pkgs.fetchFromGitHub {
         owner = "ihor-sokoliuk";
         repo = "mcp-searxng";
         tag = "v${finalAttrs.version}";
-        hash = "sha256-R6pJ5fJ0V46rY7XZBhf7RDK5I3kVlqbbaxqtfm6XKkU=";
+        hash = "sha256-M3VfUAxocp+Trj68WofTXwMAxBcD2j5bzb2mmNEPnAE=";
       };
-      npmDepsHash = "sha256-3Mbhmd9Rw6k5OcbcfJf7JKXtgpke5GiDkf0CqY9YYAw=";
+      npmDepsHash = "sha256-8R1DJ4S/q6ZLxPMVcBKTE0lJre5KAssWTreG4yNKZFw=";
       meta.mainProgram = "mcp-searxng";
     });
   };
